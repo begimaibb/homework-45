31628c60a22c963f900d2c468517362cc1b63681
begimaib <begimai.bolotbekova>
2022-07-01 18:52:20 +0600

    the changes were done in files such as urls.py, task_view.html etc. due to the creation of the partial template and new functions

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from webapp.views import index_view, create_task, task_view
+from webapp.views import index_view, create_task, task_view, update_task, delete_task
 
 
 urlpatterns = [
     path('', index_view, name="index"),
     path('tasks/add/', create_task, name="create"),
-    path('task/<int:pk>/', task_view, name="task_view")
+    path('task/<int:pk>/', task_view, name="task_view"),
+    path('task/<int:pk>/update', update_task, name="update_task"),
+    path('task/<int:pk>/delete', delete_task, name="delete_task"),
 ]
