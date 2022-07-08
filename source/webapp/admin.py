fa0ed51b0e5ecde01bc8160003b9e118e316d45e
begimaib <begimai.bolotbekova>
2022-07-08 14:55:37 +0600

    new models such as Status and Type are added, admin panel changed accordingly

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -5,11 +5,11 @@ from webapp.models import Task
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'description', 'status', 'date']
+    list_display = ['id', 'name', 'description', 'status', 'type', 'created_at', 'updated_at']
     list_display_links = ['name']
-    list_filter = ['date']
+    list_filter = ['status']
     search_fields = ['name', 'description']
-    fields = ['name', 'description', 'status', 'date']
+    fields = ['name', 'description', 'status', 'type', 'created_at', 'updated_at']
 
 
 admin.site.register(Task, TaskAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -3,19 +3,49 @@ from django.db import models
 
 # Create your models here.
 
-class Task(models.Model):
-    status_choices = [('new', 'New'), ('in_progress', 'In progress'), ('done', 'Done')]
-    name = models.CharField(max_length=50, null=False, blank=False, verbose_name="Name")
-    description = models.TextField(max_length=100, null=True, blank=True, verbose_name="Description")
-    status = models.CharField(max_length=50, choices=status_choices, default='1')
-    date = models.DateField(max_length=50, null=True, blank=True, verbose_name="Date")
 
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Date created")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Date updated")
+
+    class Meta:
+        abstract = True
+
+
+class Status(BaseModel):
+    status_name = models.CharField(max_length=50, null=False, blank=False, verbose_name="Status")
 
     def __str__(self):
-        return f"{self.id}. {self.name}, {self.description} {self.status} {self.date}"
+        return f"{self.id}. {self.status_name}"
+
+    class Meta:
+        db_table = "statuses"
+        verbose_name = "Status"
+        verbose_name_plural = "Statuses"
+
+
+class Type(BaseModel):
+    type_name = models.CharField(max_length=50, null=False, blank=False, verbose_name="Type")
+
+    def __str__(self):
+        return f"{self.id}. {self.type_name}"
+
+    class Meta:
+        db_table = "types"
+        verbose_name = "Type"
+        verbose_name_plural = "Types"
+
+
+class Task(BaseModel):
+    name = models.CharField(max_length=50, null=False, blank=False, verbose_name="Name")
+    description = models.TextField(max_length=100, null=True, blank=True, verbose_name="Description")
+    status = models.ForeignKey("webapp.Status", on_delete=models.CASCADE, related_name='status')
+    type = models.ForeignKey("webapp.Type", on_delete=models.CASCADE, related_name='type')
+
+    def __str__(self):
+        return f"{self.id}. {self.name}, {self.description} {self.status} {self.type}"
 
     class Meta:
         db_table = "tasks"
         verbose_name = "Task"
         verbose_name_plural = "Tasks"
-
