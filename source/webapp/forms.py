7c66793de647b3d67a1d81dd9b0f370f4434fb99
begimaib <begimai.bolotbekova>
2022-07-19 09:38:23 +0600

    added validation to name and description

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -17,7 +17,8 @@ class TaskForm(forms.ModelForm):
     def clean(self):
         name = self.cleaned_data.get("name")
         description = self.cleaned_data.get("description")
-        if not re.match("[a-zA-Zа-яА-Я]+$", name):
+        if not re.match("^[a-zA-Zа-яА-Я\s]+$", name):
             self.add_error("name", ValidationError("The name should include only letters"))
-        if not re.match("a-zA-Zа-яА-Я]+$", description):
+        if not re.match("^[a-zA-Zа-яА-Я\s]+$", description):
             self.add_error("description", ValidationError("The description should include only letters"))
+        return super().clean()
