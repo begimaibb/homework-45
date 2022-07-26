efbfbcc6b365a0092c1176819cd9b5ca9469da98
begimaib <begimai.bolotbekova>
2022-07-26 15:44:35 +0600

    new links are added to the urls.py

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.views.generic import TemplateView, RedirectView
 
 from webapp.views import IndexView, CreateTask, TaskView, UpdateTask, DeleteTask
+from webapp.views.projects import ProjectIndexView, ProjectView
 
 
 urlpatterns = [
@@ -11,4 +12,6 @@ urlpatterns = [
     path('task/<int:pk>/', TaskView.as_view(), name="task_view"),
     path('task/<int:pk>/update', UpdateTask.as_view(), name="update_task"),
     path('task/<int:pk>/delete', DeleteTask.as_view(), name="delete_task"),
+    path('projects/', ProjectIndexView.as_view(), name="project_index"),
+    path('project/<int:pk>/', ProjectView.as_view(), name="project_view"),
 ]
