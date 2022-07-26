0f3ee41de40c82f317998b88f3e381ebd1e8693c
begimaib <begimai.bolotbekova>
2022-07-26 18:52:54 +0600

    urls for projects are added, home page is switched to project_index

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
-from django.views.generic import TemplateView, RedirectView
 
 from webapp.views import IndexView, CreateTask, TaskView, UpdateTask, DeleteTask
-from webapp.views.projects import ProjectIndexView, ProjectView
+from webapp.views.projects import ProjectIndexView, ProjectView, CreateProjectView
 
 
 urlpatterns = [
-    path('', IndexView.as_view(), name="index"),
-    path('tasks/', RedirectView.as_view(pattern_name="index")),
+    path('tasks/', IndexView.as_view(), name="index"),
     path('tasks/add/', CreateTask.as_view(), name="create"),
     path('task/<int:pk>/', TaskView.as_view(), name="task_view"),
     path('task/<int:pk>/update', UpdateTask.as_view(), name="update_task"),
     path('task/<int:pk>/delete', DeleteTask.as_view(), name="delete_task"),
-    path('projects/', ProjectIndexView.as_view(), name="project_index"),
+    path('', ProjectIndexView.as_view(), name="project_index"),
     path('project/<int:pk>/', ProjectView.as_view(), name="project_view"),
+    path('projects/add/', CreateProjectView.as_view(), name="create_project"),
 ]
