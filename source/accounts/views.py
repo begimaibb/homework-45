706bd7a4c2dd743294886b1e206186bd2013affe
begimaib <begimai.bolotbekova>
2022-08-09 18:15:46 +0600

    added view for registration

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,9 +1,34 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from accounts.forms import MyUserCreationForm
 
 
 # Create your views here.
+
+class RegisterView(CreateView):
+    model = User
+    template_name = 'registration.html'
+    form_class = MyUserCreationForm
+
+    def form_valid(self,form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('webapp:index')
+        return next_url
+
+
 def login_view(request):
     context = {}
     if request.method == 'POST':
@@ -21,5 +46,7 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('webapp:project_index')
+
+
 
 
